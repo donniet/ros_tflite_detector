55dbd6fb89d622fa5153394cda6de2730d1dadc4
Donnie <donniet@gmail.com>
2020-08-28 15:59:46 -0500

    treat a box like tensorflow

--- scripts/detect.py
+++ scripts/detect.py
@@ -3,38 +3,64 @@
 import argparse
 from functools import partial
 
+import io
 import numpy as np
 import tflite_runtime.interpreter as tflite
+import PIL.Image
+import PIL.ImageDraw
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CompressedImage
 from tflite_detector.msg import Detect, Box
 
-def process(interpreter, input_details, output_details, publisher, image):
+frame = 0
+
+def process(interpreter, input_details, output_details, publisher, annotations, image):
+    global frame
     dat = np.frombuffer(image.data, dtype=np.uint8)
     dat = dat.reshape(input_details[0]['shape'])
 
     interpreter.set_tensor(input_details[0]['index'], dat)
     interpreter.invoke()
 
+    im = PIL.Image.fromarray(dat[0])
+    draw = PIL.ImageDraw.Draw(im)
+    
+
     det = Detect()
-    det.num = int(interpreter.get_tensor(output_details[3]['index']))
-    det.scores = interpreter.get_tensor(output_details[2]['index'])
+    det.num = int(interpreter.get_tensor(output_details[3]['index'])[0])
+    det.scores = interpreter.get_tensor(output_details[2]['index'])[0][0:det.num]
     #det.classes implement this 
     det.boxes = []
-    detection_boxes = interpreter.get_tensor(output_details[0]['index'])
+    detection_boxes = interpreter.get_tensor(output_details[0]['index'])[0]
     if det.num > 0:
-        for i in range(len(detection_boxes)):
+        for i in range(det.num):
             box = Box()
-            box.top = detection_boxes[i][0]
-            box.left = detection_boxes[i][1]
-            box.bottom = detection_boxes[i][2]
-            box.right = detection_boxes[i][3]
+            box.coords = detection_boxes[i]
             det.boxes.append(box)
 
+            if det.scores[i] > 0.5:
+                bb = np.array(detection_boxes[i])*320
+
+                draw.rectangle((bb[1], bb[0], bb[3], bb[2]))
+
+    with io.BytesIO() as output:
+        im.save(output, format='JPEG')
+        compressed = CompressedImage()
+        compressed.header.stamp = rospy.Time.now()
+        compressed.format = 'jpeg'
+        compressed.data = output.getvalue()
+
+#        with open('/tmp/frame{0:05d}.jpg'.format(frame), 'wb') as f:
+#            f.write(output.getvalue())
+#            frame = (frame + 1) % 10
+
     publisher.publish(det)
+    annotations.publish(compressed)
+
+
         
-    print('processing image, found: {}'.format(det.num))
+    #print('processing image, found: {}'.format(det.num))
 
 def listener(args):
     if args.edgetpu:
@@ -47,6 +73,8 @@ def listener(args):
             model_path = args.model_path
         )
 
+
+
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
@@ -57,8 +85,9 @@ def listener(args):
     rospy.init_node('detector', anonymous=True)
 
     publisher = rospy.Publisher('detections', Detect, queue_size=100)
+    annotations = rospy.Publisher('annotations', CompressedImage, queue_size=100)
 
-    rospy.Subscriber('rawFrames', Image, partial(process, interpreter, input_details, output_details, publisher))
+    rospy.Subscriber('rawFrames', Image, partial(process, interpreter, input_details, output_details, publisher, annotations))
 
     rospy.spin()
 
@@ -66,6 +95,7 @@ def listener(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_path', help='directory to saved_model', default='.')
+    parser.add_argument('--label_map', help='path to label_map file', default='label_map.pbtxt')
     parser.add_argument('--edgetpu', help='load edgetpu library', type=bool, default=True)
     
     listener(parser.parse_args())
